256f73e5cd5009038dbdc2d2bf410facb76c00ad
Courtney Van Breda <cmvanbreda91@gmail.com>
2022-11-07 21:17:29 -0500

    add dependancies and update client side

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,12 +17,57 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
+    plugins: [    
+      new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'JATE',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+        name: 'just another text editor',
+        short_name: 'jATE',
+        description: 'keep notes and code updates in once place',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }), 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }), 
       
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
         
       ],
     },
